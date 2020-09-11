262c9ceee9a3c5e4f045802af692a8371fefba07
Layon Martins <layonmartins03@gmail.com>
2020-09-11 15:21:48 -0300

    change theme style to red

--- app/src/main/java/com/layon/android/androidnotificationtimesentive/FullScreenActivity.kt
+++ app/src/main/java/com/layon/android/androidnotificationtimesentive/FullScreenActivity.kt
@@ -12,18 +12,23 @@ class FullScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen)
 
+        window.decorView.apply{
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
         //hide the navigation bar
-        val decorView = window.decorView
-        val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        //val decorView = window.decorView
+        //val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         //| View.SYSTEM_UI_FLAG_FULLSCREEN;
         //| View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.systemUiVisibility = uiOptions
+        //decorView.systemUiVisibility = uiOptions
 
         //change statusbar color
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.statusBarColor = Color.WHITE
-        }
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //window.statusBarColor = Color.WHITE
+        //}
 
         Log.i("layonf", "create incoming call")
     }
