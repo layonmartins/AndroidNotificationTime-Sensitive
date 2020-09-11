fa82c00fcc644c6a68f985b5e5be07b73aff307a
Layon Martins <layonmartins03@gmail.com>
2020-09-11 11:51:11 -0300

    first commit

---
+++ app/src/main/java/com/layon/android/androidnotificationtimesentive/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.layon.android.androidnotificationtimesentive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
