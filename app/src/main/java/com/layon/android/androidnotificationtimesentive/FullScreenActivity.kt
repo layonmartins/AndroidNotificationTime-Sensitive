e5d1d2504750ef91b813179776bdbefe1692258c
Layon Martins <layonmartins03@gmail.com>
2020-09-14 13:36:01 -0300

    start the full screen activity over keyguard and screen off

--- app/src/main/java/com/layon/android/androidnotificationtimesentive/FullScreenActivity.kt
+++ app/src/main/java/com/layon/android/androidnotificationtimesentive/FullScreenActivity.kt
@@ -1,9 +1,13 @@
 package com.layon.android.androidnotificationtimesentive
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_full_screen.*
 
@@ -13,6 +17,15 @@ class FullScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen)
 
+        //start the activity over keyguard and screen off
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+        }
+
+        //disable navigation and statusbar
         window.decorView.apply{
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -20,6 +33,7 @@ class FullScreenActivity : AppCompatActivity() {
 
         Log.i("layonf", "create incoming call")
 
+        //go to start activity when touch in fullscreen activity
         fullscreenactivity.setOnClickListener({
             Log.i("layonf", "touch")
             val i = Intent(this, MainActivity::class.java)
